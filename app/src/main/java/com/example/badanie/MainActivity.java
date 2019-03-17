8d4a8db52992755554aebcea1077f43b27078f26
kb861 <katabie861@student.polsl.pl>
2019-03-17 19:00:30 +0100

    StartActivity

--- app/src/main/java/com/example/badanie/MainActivity.java
+++ app/src/main/java/com/example/badanie/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.badanie;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
+    @BindView(R.id.btn_ID)
+    Button btn_ID;
+
+    @OnClick(R.id.btn_ID)
+    void onClick(View view) {
+        Intent intent = new Intent(this, StartActivity.class);
+        Bundle bundle = new Bundle();
+        intent.putExtras(bundle);
+        startActivity(intent);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
