60b185c13feaff344bdc0ee5237a643ab4ae960d
kb861 <katabie861@student.polsl.pl>
2019-03-20 12:34:06 +0100

    timer start stop and lap

--- app/src/main/java/com/example/badanie/Chronometer.java
+++ app/src/main/java/com/example/badanie/Chronometer.java
@@ -4,7 +4,6 @@ import android.content.Context;
 
 public class Chronometer implements Runnable {
 
-    //Some constants for milliseconds to hours, minutes, and seconds conversion
     public static final long MILLIS_TO_MINUTES = 60000;
     public static final long MILLS_TO_HOURS = 3600000;
 
@@ -77,23 +76,19 @@ public class Chronometer implements Runnable {
     @Override
     public void run() {
         while(mIsRunning) {
-            //We do not call ConvertTimeToString here because it will add some overhead
-            //therefore we do the calculation without any function calls!
 
-            //Here we calculate the difference of starting time and current time
+
             long since = System.currentTimeMillis() - mStartTime;
 
-            //convert the resulted time difference into hours, minutes, seconds and milliseconds
             int seconds = (int) (since / 1000) % 60;
             int minutes = (int) ((since / (MILLIS_TO_MINUTES)) % 60);
-            //int hours = (int) ((since / (MILLS_TO_HOURS)) % 24); //this resets to  0 after 24 hour!
-            int hours = (int) ((since / (MILLS_TO_HOURS))); //this does not reset to 0!
-            int millis = (int) since % 1000; //the last 3 digits of millisecs
 
-//            ((MainActivity) mContext).updateTimerText(String.format("%02d:%02d:%02d:%03d"
-////                    , hours, minutes, seconds, millis));
+            int hours = (int) ((since / (MILLS_TO_HOURS)));
+            int millis = (int) since % 1000;
 
-            //Sleep the thread for a short amount, to prevent high CPU usage!
+           ((StopwatchActivity) mContext).updateTimerText(String.format("%02d:%02d:%02d:%03d"
+                 , hours, minutes, seconds, millis));
+
             try {
                 Thread.sleep(15);
             } catch (InterruptedException e) {
--- app/src/main/java/com/example/badanie/StopwatchActivity.java
+++ app/src/main/java/com/example/badanie/StopwatchActivity.java
@@ -1,15 +1,23 @@
 package com.example.badanie;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by patry on 17.03.2019.
@@ -23,23 +31,102 @@ public class StopwatchActivity extends AppCompatActivity {
     @BindView(R.id.btn_Next)
     Button btn_Next;
 
-
+    @BindView(R.id.TextViewTime)
+    TextView TextViewTime;
 
     @BindView(R.id.TVName)
     TextView TVName;
 
+    @BindView(R.id.et_laps)
+    EditText et_laps;
+
+    @BindView(R.id.sv_lap)
+    ScrollView sv_lap;
+
+    int mLapCounter = 1;
+
+    Chronometer Chrono;
+
+
+    Thread ThreadChrono;
+
+    Context Context;
+
+    @OnClick(R.id.btn_Start)
+    void onClick(View view) {
+        if (Chrono != null) {
+
+            Chrono.stop();
+            ThreadChrono.interrupt();
+            ThreadChrono = null;
+            Chrono = null;
+        }
+    }
+
+    @OnClick(R.id.btn_Next)
+    void onClickNext(View view) {
+        if (Chrono == null) {
+            Toast.makeText(Context
+                    , "nic", Toast.LENGTH_SHORT).show();
+            return; //do nothing!
+        }
+
+        //we just simply copy the current text of tv_timer and append it to et_laps
+        et_laps.append("LAP " + String.valueOf(mLapCounter++)
+                + "   " + TextViewTime.getText() + "\n");
+
+        //scroll to the bottom of et_laps
+        sv_lap.post(new Runnable() {
+            @Override
+            public void run() {
+                sv_lap.smoothScrollTo(0, et_laps.getBottom());
+            }
+        });
+    }
+
+
     public List<String> tasks = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
         ButterKnife.bind(this);
-
+        Context = this;
         tasks.add("Zadanie1");
         tasks.add("Zadanie2");
         tasks.add("Zadanie3");
         TVName.setText(tasks.get(0));
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
 
+
+        //starttime
+        if (Chrono == null) {
+            //instantiate the chronometer
+            Chrono = new Chronometer(Context);
+            //run the chronometer on a separate thread
+            ThreadChrono = new Thread(Chrono);
+            ThreadChrono.start();
+
+            //start the chronometer!
+            Chrono.start();
+
+            //clear the perilously populated et_laps
+            et_laps.setText(""); //empty string!
+
+            //reset the lap counter
+            mLapCounter = 1;
+        }
+
+    }
+
+    public void updateTimerText(final String timeAsText) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                TextViewTime.setText(timeAsText);
+            }
+        });
     }
 
 
