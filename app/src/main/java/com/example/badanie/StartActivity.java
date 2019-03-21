304e7dd20a3398c12c2f15dea9e23da69efa8489
matecebr <mateceb384@student.polsl.pl>
2019-03-22 00:59:42 +0100

    Sprawdzenie ID

--- app/src/main/java/com/example/badanie/StartActivity.java
+++ app/src/main/java/com/example/badanie/StartActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -30,11 +31,20 @@ public class StartActivity extends AppCompatActivity {
 
     @OnClick(R.id.btn_ID)
     void onClick(View view) {
-        Intent intent = new Intent(this, StopwatchActivity.class);
-        Bundle bundle = new Bundle();
-        intent.putExtras(bundle);
-        startActivity(intent);
 
+        String value= EtID.getText().toString();
+        int finalValue=Integer.parseInt(value);
+
+        if ( finalValue >=101 && finalValue<=201 )
+        {
+            Intent intent = new Intent(this, StopwatchActivity.class);
+            Bundle bundle = new Bundle();
+            intent.putExtras(bundle);
+            startActivity(intent);
+        }
+        else{
+            Toast.makeText(StartActivity.this, "Error wrong Id!", Toast.LENGTH_SHORT).show();
+        }
 
     }
     @Override
